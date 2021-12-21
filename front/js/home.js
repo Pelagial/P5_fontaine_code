d81e17b54f356543f22565536c589d81ed7c5a05
Allan Fontaine <pelagialstudio@outlook.com>
2021-12-21 15:37:44 +0100

    home.js add to project and config home page work

---
+++ front/js/home.js
@@ -0,0 +1,31 @@
+/**
+ * API Products Data collect and display, switch to product page
+ */
+
+/** API Products Data collect */
+let productsData = [];
+
+const listProducts = async () => {
+    await fetch("http://localhost:3000/api/products")
+    .then(data => data.json())
+    .then((promiseData) => {
+        productsData = promiseData;
+        console.log(productsData);
+    });  
+};
+
+/** API Products Data display */
+const productsDisplay = async () =>{
+    await listProducts();
+    document.getElementById("items").innerHTML = productsData.map((products) => 
+    `<a href="./product.html?id=${products._id}">
+    <article>
+      <img src="${products.imageUrl}" alt="${products.altTxt}">
+      <h3 class="productName">${products.name}</h3>
+      <p class="productDescription">${products.description}</p>
+    </article>
+    </a>`).join("");
+};
+
+productsDisplay();
+
