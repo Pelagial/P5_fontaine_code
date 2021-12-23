fea642f4eca90a923eb5faba7ee3089e05a98292
Allan Fontaine <pelagialstudio@outlook.com>
2021-12-23 16:10:55 +0100

    add js basket functions to product page

--- front/js/product.js
+++ front/js/product.js
@@ -1,27 +1,24 @@
 /**
- * Product Id Data collect and display for product page
+ * Products Data collect and display for product page
  */
 
-/** Products ID Data collect */
-/** Current Page Id info collect */
+/** Products Data collect */
+/** Current Page Id data collect */
 var urlcourante = document.location.href; 
 var url = new URL(urlcourante);
 var id = url.searchParams.get("id");
 console.log(id);
 
-/** Current Product Id info collect */
-let productDetails = [];
-
+/** Current Product data collect */
 const detailsProduct = async () => {
     await fetch("http://localhost:3000/api/products/"+ id)
     .then(details => details.json())
     .then((promiseDetails) => {
         productDetails = promiseDetails;
-        console.log(productDetails);
     });  
 };
 
-/** API Products Data display */
+/** Products data display */
 const displayDetailsProduct = async () =>{
     await detailsProduct();
 
@@ -45,5 +42,59 @@ const displayDetailsProduct = async () =>{
 };
 
 displayDetailsProduct();
-/** 
- * document.getElementById("").innerHTML */
+
+/** Add to basket functions*/
+function saveBasket(basket){
+    localStorage.setItem("basket", JSON.stringify(basket));
+};
+
+function selectedProd(){
+    detailsProduct();
+    let productData = {    
+        id : productDetails._id, 
+        imageUrl: productDetails.imageUrl,
+        altTxt: productDetails.altTxt,
+        name : productDetails.name,
+        price : productDetails.price,
+        colorSelected : document.getElementById("colors").value,
+        quantitySelected : document.getElementById("quantity").value
+    };
+    return productData
+};
+
+function getBasket(){
+    let basket = localStorage.getItem("basket");
+    if (basket == null){
+        return [];
+    }
+    else{
+        return JSON.parse(basket);
+    }
+};
+
+function addTobasket(){
+    let basket = getBasket();
+    basketProducts = selectedProd();
+    let foundProduct = basket.find(i => i.id == basketProducts.id);
+    console.log(foundProduct);
+    let foundColor =  basket.find(c => c.colorSelected == basketProducts.colorSelected);
+    console.log(foundColor);
+    if(foundProduct != undefined){
+        if(foundColor != undefined){
+            foundColor.quantitySelected++;
+        }else{
+            basketProducts = selectedProd();
+            basket.push(basketProducts);
+        }
+    }
+    else{
+        basketProducts = selectedProd();
+        basket.push(basketProducts);
+    };
+    saveBasket(basket);
+};
+
+document.getElementById("addToCart")
+    .addEventListener("click", () => {
+        addTobasket()
+});
